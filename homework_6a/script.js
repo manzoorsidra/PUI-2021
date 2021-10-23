37099b6a8ec5b85a5449099f04ed3cdcf4fb7404
Sidra <smanzoor@andrew.cmu.edu>
2021-10-23 00:07:24 -0400

    Added List Creation + Cart Counter + Change in Visuals

--- homework_6a/script.js
+++ homework_6a/script.js
@@ -1,37 +1,63 @@
-document.getElementById("addToCartBtn").addEventListener("click", function() {
-    for(var i=1;i<6;i++) {
-        document.getElementsByClassName(i+'_p').style.backgroundColor = '#BEDEB6';
+//Capture selected values of radio group and add to "items" array
+var items = [];
+const cartBtn = document.querySelector('addToCartBtn');
+addToCartBtn.onclick = function () {
+    const rbs = document.querySelectorAll('input[name="glaze_flavor"]');
+    const rbs2 = document.querySelectorAll('input[name="quantity"]');
+
+    let selectedValue1;
+    let selectedValue2;
+
+    for (const rb of rbs) {
+        if (rb.checked) {
+            selectedValue1 = rb.value;
+        }
+    }
+
+    for (const rb2 of rbs2) {
+      if (rb2.checked) {
+          selectedValue2 = rb2.value;
       }
-  });
+  }
+    var item = [selectedValue1, selectedValue2];
+    items.push(item);
+    console.log(items);
+
+//counter for the shopping cart
+    for (var i = 0; i <= items.length; i++){
+      document.getElementById("disp").innerHTML = i;
+    }
+};
+
+
+//Based on which option is selected change image
+document.body.addEventListener('change', function(e){
+  let target = e.target;
+  let message;
+  switch (target.id) {
+      case 'vanillaMilk':
+        //Add image src attributes here to change images based on selection
+          document.getElementById("rollimage").src = "assets/images/walnut.png";
+          message = 'vanillaMilk';
+          break;
+      case 'sugarMilk':
+          document.getElementById("rollimage").src = "assets/images/originaldetail.png";
+          message = 'sugarMilk';
+          break;
+      case 'doubleChocolate':
+          document.getElementById("rollimage").src = "assets/images/pumpkinspice.png";
+          message = 'doubleChocolate';
+          break;
+      case 'noGlaze':
+          document.getElementById("rollimage").src = "assets/images/caramelpecan.png";
+          message = 'noGlaze';
+          break;
+  }
+  console.log(message);
+});
 
 
 
-function addToCart() { 
-    if(document.getElementById('noGlaze').checked) {
-        document.getElementById("disp").innerHTML
-            = document.getElementById("noGlaze").value
-            + " radio button checked";
-    }
-    else if(document.getElementById('vanillaMilk').checked) {
-        document.getElementById("disp").innerHTML
-            = document.getElementById("vanillaMilk").value
-            + " radio button checked";  
-    }
-    else if(document.getElementById('sugarMilk').checked) {
-        document.getElementById("disp").innerHTML
-            = document.getElementById("sugarMilk").value
-            + " radio button checked";  
-    }
-    else if(document.getElementById('doubleChocolate').checked) {
-        document.getElementById("disp").innerHTML
-            = document.getElementById("doubleChocolate").value
-            + " radio button checked";  
-    }
-    else {
-        document.getElementById("disp").innerHTML
-            = "No one selected";
-    }
-}
 
 
 
